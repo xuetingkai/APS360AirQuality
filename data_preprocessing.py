505ffc9ac8fd6b2d9d9b6a0071c10d94c923a338
Greta <g.a.skorupska-ruiz@student.tudelft.nl>
2023-10-23 19:07:11 -0400

    ??

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,5 +1,4 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
-data = pd.read_csv('/Data/weatherdata/weatherstats_toronto_normal_daily.csv')
+data = pd.read_csv('\Data\weather_data\weatherstats_toronto_normal_daily.csv')
