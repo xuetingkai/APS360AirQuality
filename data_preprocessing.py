e22987799f95a8058e610cd7151ee242b34b6f68
Greta <g.a.skorupska-ruiz@student.tudelft.nl>
2023-10-23 19:29:40 -0400

    Update data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -3,6 +3,11 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 data = pd.read_csv('Data\weather_data\weatherstats_toronto_normal_daily.csv')
-pollution_data_1 = pd.read_csv('Data\PM2.5\PM2.5_2003_2012.csv')
-pollution_data_2 = pd.read_csv('Data\PM2.5\PM2.5_2013_2022.csv')
-print(data.head())
+
+pollution_data_1 = pd.read_csv('Data\PM2.5\PM2.5_2003_2012.csv', skiprows=10)
+pollution_data_2 = pd.read_csv('Data\PM2.5\PM2.5_2013_2022.csv', skiprows=10)
+
+pollution_data = pd.concat([pollution_data_1, pollution_data_2], axis=0)
+
+
+print(data)
