656c7c7203b8bb01ad2686edbba6ff96981c2271
Greta <g.a.skorupska-ruiz@student.tudelft.nl>
2023-10-23 19:09:45 -0400

    Update data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,4 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-data = pd.read_csv('\Data\weather_data\weatherstats_toronto_normal_daily.csv')
+
+data = pd.read_csv('Data\weather_data\weatherstats_toronto_normal_daily.csv')
+pollution_data = pd.read_csv('Data\PM2.5\pollution_data.csv')
+
+print(data.head())
